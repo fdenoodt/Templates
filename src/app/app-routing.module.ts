6b06add1e3c3e08675b2fb5c69f913e43c6552fc
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-06-30 13:32:57 +0200

    the first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { StartComponent } from './start/start.component';
+import { SignInComponent } from './signin/signin.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: 'auth', component: SignInComponent },
+  { path: 'main', component: MainComponent },
+  { path: '', component: StartComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { SignInComponent } from './signin/signin.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartComponent,
+    SignInComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

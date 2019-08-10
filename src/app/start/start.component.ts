60664dce7ccab0178edaae47b1093482b88dee2f
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-08-10 21:03:27 +0200

    autologin

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-start',
@@ -8,10 +9,14 @@ import { Router } from '@angular/router';
 })
 export class StartComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
-    this.router.navigate(['/main'])
+    if (this.authService.isAuthenticated)
+      this.router.navigate(['/auth'])
+    else {
+      this.router.navigate(['/main'])
+    }
   }
 
 }
